540cf22a5fad42729c4d34c81ab80d164b8c7f6e
Evan2502 <marteauevan25@gmail.com>
2024-07-18 22:19:49 +0200

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,9 @@
+const MenuDeroulant = document.getElementById("menuDeroul");
+
+function menu() {
+    MenuDeroulant.style.visibility = "visible";
+}
+
+function menuferm(){
+    MenuDeroulant.style.visibility = "hidden";
+}
